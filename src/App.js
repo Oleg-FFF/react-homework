18c2c971331fe8df46c4bfc6fe888b230ca5e4a0
TarasLesh <t.leshchyshyn@gmail.com>
2020-03-21 19:07:28 +0200

    Added button backlight

--- src/App.js
+++ src/App.js
@@ -104,9 +104,10 @@ class App extends Component {
                                         <div className="all-posts">
                                             {
                                                 posts.map((post) => {
+                                                    console.log('wwww');
                                                     return (
                                                         <Post post={post} key={post.id}/>
-                                                        console.log('11')
+
                                                     );
                                                 })
                                             }
