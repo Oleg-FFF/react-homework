3c8a60d3a8a5168c2123f36e12554218d0908a93
mzhehalo <mzhehalo@gmail.com>
2020-03-21 23:36:48 +0200

    Merge branch 'remote-lesson-1' of https://github.com/AlyonaPianykh/react-homework into mzhehalo
     Conflicts:
    	src/App.js

--- src/components/AddUserForm/AddUserForm.js
+++ src/components/AddUserForm/AddUserForm.js
@@ -11,7 +11,11 @@ class AddUserForm extends Component {
         super(props);
 
         this.state = {
-            currentError: ''
+            currentError: '',
+            inputErrorName: '',
+            inputErrorLastName: '',
+            errorInputName: '',
+            errorInputLastName: ''
         };
     }
 
@@ -26,8 +30,25 @@ class AddUserForm extends Component {
     onInputChange = (e) => {
 
         if (e.target.value) {
-            this.setState({currentError: ''});
+            this.setState({
+                currentError: ''
+            })
         }
+
+        if (this.userNameRef.current.value) {
+            this.setState({
+                inputErrorName: '',
+                errorInputName: ''
+            })
+        }
+
+        if (this.userLastNameRef.current.value) {
+            this.setState({
+                inputErrorLastName: '',
+                errorInputLastName: ''
+            })
+        }
+
         // todo: если в стейте была записана какая-то ошибка - зачищаем ее при изменении инпута
         //  поскольку в ситуации если пользователь ничего - не ввел - увидел ошибку - понял, что не так - начал вводить данные
         //  было бы хорошо не пугать его висящим сообщением, что все таки что-то не так
@@ -36,15 +57,33 @@ class AddUserForm extends Component {
     onSubmit = (e) => {
         e.preventDefault();
 
-        if (this.userNameRef.current.value === '' && this.userLastNameRef.current.value === '') {
+        if (this.userNameRef.current.value.trim() === '' && this.userLastNameRef.current.value.trim() === '') {
             this.setState({
-                currentError: <div className='warning'>
-                    <div>
-                        <h2>WARNING!!! WRITE SOMETHING!!!</h2>
-                        <img src={ImgWarning} alt="Warning"/>
-                    </div>
-                </div>
+                currentError:
+                    <div className='warning'>
+                        <div>
+                            <h2>WARNING!!! WRITE SOMETHING!!!</h2>
+                            <img src={ImgWarning} alt="Warning"/>
+                        </div>
+                    </div>,
             });
+        }
+
+        if (!this.userNameRef.current.value.trim()) {
+
+            this.setState({
+                inputErrorName: 'second-form-control',
+                errorInputName: 'This input is empty! Write something!'
+
+            })
+        }
+
+        if (!this.userLastNameRef.current.value.trim()) {
+
+            this.setState({
+                inputErrorLastName: 'second-form-control',
+                errorInputLastName: 'This input is empty! Write something!'
+            })
         }
 
         // Todo: добавить здесь проверку, если пользователь НЕ ВВЕЛ в инпуты ничего - не создаем пост
@@ -96,21 +135,27 @@ class AddUserForm extends Component {
                     <label htmlFor="title" className="input-group-text">Enter user name:</label>
                     <input
                         ref={this.userNameRef}
-                        className="form-control"
+                        className={`form-control ${this.state.inputErrorName}`}
                         type="text"
                         name="userName"
                         onChange={this.onInputChange}
                     />
+                    <div className="errorInputs">
+                        {this.state.errorInputName}
+                    </div>
                 </div>
                 <div className="form-group">
                     <label htmlFor="text" className="input-group-text">Enter user last name:</label>
                     <input
                         ref={this.userLastNameRef}
-                        className="form-control"
+                        className={`form-control ${this.state.inputErrorLastName}`}
                         type="text"
                         name="userLastName"
                         onChange={this.onInputChange}
                     />
+                    <div className="errorInputs">
+                        {this.state.errorInputLastName}
+                    </div>
                 </div>
 
                 {this.state.currentError}
@@ -119,9 +164,13 @@ class AddUserForm extends Component {
                 {/* todo: стилизуйте это сообщение об ошибке, чтоб текст был красным и броским */}
                 {/* todo: можете добавить иконку с восклицательным знаком, чтоб привлечь внимание пользователя */}
                 {/* todo: под ошибкой имеется ввиду, что пользователь не ввел данные в инпуты и нажал submit */}
-                <Button type="submit" className="btn-outline-secondary" label="Add post"/>
+                <Button type="submit"
+                        className="btn-outline-secondary"
+                        label="Add post"
+                />
 
-                <Button className="btn-outline-secondary" label="Reset"
+                <Button className="btn-outline-secondary"
+                        label="Reset"
                         onClick={this.onReset}
                 />
                 {/* ToDo: добавить кнопку для скидывания введеных пользователем значений */}
